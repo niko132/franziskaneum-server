80d882194d8dd92d0409faa18f369c0e0386e245
niko132 <nikokirste@hotmail.de>
2018-03-15 23:35:23 +0100

    .

--- web/update.php
+++ web/update.php
@@ -3,7 +3,7 @@
 		$token = $_POST['token'];
 		echo $token;
 		echo '\n';
-	} else {
+		
 		$db_connection = pg_connect("host=ec2-54-75-227-92.eu-west-1.compute.amazonaws.com dbname=d7i5315uupgr2m user=mydxnmvauddyee password=493c42d4734c867c3fda62972ff79b1df2994deea5af051b3b1a4052e89f0381");
 		
 		if (isset($_POST['old_token']) && !empty($_POST['old_token'])) {
