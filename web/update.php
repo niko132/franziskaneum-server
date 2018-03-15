3f61116872944c7cb88997cf8943aae21096232f
niko132 <nikokirste@hotmail.de>
2018-03-15 23:46:51 +0100

    .

--- web/update.php
+++ web/update.php
@@ -6,20 +6,20 @@
 		
 		$db_connection = pg_connect("host=ec2-54-75-227-92.eu-west-1.compute.amazonaws.com dbname=d7i5315uupgr2m user=mydxnmvauddyee password=493c42d4734c867c3fda62972ff79b1df2994deea5af051b3b1a4052e89f0381");
 		
-		$aaa = $_POST['old_token'];
-		echo $aaa;
-		echo ' ';
+		$old_token = $_POST['old_token'];
+		$is_teacher = $_POST['is_teacher'];
+		$school_class = $POST['school_class'];
+		$school_class_index = $POST['school_class_index'];
+		$teacher_shortcut = $POST['teacher_shortcut'];
+		$courses = $POST['courses'];
 		
-		if (isset($_POST['old_token']) && !empty($_POST['old_token'])) {
-			$old_token = $_POST['old_token'];
-			echo $old_token;
-			echo '\n';
+		if (!empty($old_token)) {
+			echo ' OLD TOKEN ';
 			
 			// update existing row
 		} else {
 			$result = pg_query($db_connection, "INSERT INTO users (token) VALUES ('" . $token . "')");
-			echo 'Hello World';
-			echo ' ';
+			echo ' NEW ONE ';
 			echo pg_last_error($db_connection);
 			// create new row
 		}
