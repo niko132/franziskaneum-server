536b4171506f59812bfe2a16f19e5950bbcd9118
niko132 <nikokirste@hotmail.de>
2018-03-15 23:27:30 +0100

    .

--- web/update.php
+++ web/update.php
@@ -1,11 +1,15 @@
 <?php
-	$token = isset($_POST['token']);
-	$old_token = isset($_POST['old_token']);
+	if (isset($_POST['token']) && !empty($_POST['token'])) {
+		$token = $_POST['token'];
+		echo $token;
+		echo '\n';
+	}
 	
-	echo $token;
-	echo '\n';
-	echo $old_token;
-	echo '\n';
+	if (isset($_POST['old_token']) && !empty($_POST['old_token'])) {
+		$old_token = $_POST['old_token'];
+		echo $old_token;
+		echo '\n';
+	}
 
     $db_connection = pg_connect("host=ec2-54-75-227-92.eu-west-1.compute.amazonaws.com dbname=d7i5315uupgr2m user=mydxnmvauddyee password=493c42d4734c867c3fda62972ff79b1df2994deea5af051b3b1a4052e89f0381");
 	
