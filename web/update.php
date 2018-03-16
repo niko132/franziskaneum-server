cbf4b334cd6b28ae7e521be5a1a8108e1e9eee53
niko132 <nikokirste@hotmail.de>
2018-03-16 16:35:06 +0100

    .

--- web/update.php
+++ web/update.php
@@ -1,9 +1,5 @@
 <?php
-	if (isset($_POST['token']) && !empty($_POST['token'])) {
-		$token = $_POST['token'];
-		echo $token;
-		echo '\n';
-		
+	if (isset($_POST['token']) && !empty($_POST['token'])) {		
 		$db_connection = pg_connect("host=ec2-54-75-227-92.eu-west-1.compute.amazonaws.com dbname=d7i5315uupgr2m user=mydxnmvauddyee password=493c42d4734c867c3fda62972ff79b1df2994deea5af051b3b1a4052e89f0381");
 		
 		$old_token = $_POST['old_token'];
@@ -57,11 +53,6 @@
 				echo '     ';
 				echo $values;
 			}
-			
-			$result = pg_query($db_connection, "INSERT INTO users (token) VALUES ('" . $token . "')");
-			echo ' NEW ONE ';
-			echo pg_last_error($db_connection);
-			// create new row
 		}
 	}
 ?>
