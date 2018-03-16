25714071a6b06174dc854e8a144f1d6c25108f17
niko132 <nikokirste@hotmail.de>
2018-03-16 16:54:54 +0100

    .

--- web/update.php
+++ web/update.php
@@ -2,15 +2,13 @@
 	if (isset($_POST['token']) && !empty($_POST['token'])) {		
 		$db_connection = pg_connect("host=ec2-54-75-227-92.eu-west-1.compute.amazonaws.com dbname=d7i5315uupgr2m user=mydxnmvauddyee password=493c42d4734c867c3fda62972ff79b1df2994deea5af051b3b1a4052e89f0381");
 		
-		echo '     ' . $_POST['school_class'] . '     ';
-		
 		$token = $_POST['token'];
 		$old_token = $_POST['old_token'];
 		$is_teacher = $_POST['is_teacher'];
-		$school_class = $POST['school_class'];
-		$school_class_index = $POST['school_class_index'];
-		$teacher_shortcut = $POST['teacher_shortcut'];
-		$courses = $POST['courses'];
+		$school_class = $_POST['school_class'];
+		$school_class_index = $_POST['school_class_index'];
+		$teacher_shortcut = $_POST['teacher_shortcut'];
+		$courses = $_POST['courses'];
 		
 		$result = pg_query($db_connection, "SELECT token FROM users WHERE token = '" . $old_token . "'");
 		$num_rows = pg_num_rows($result);
