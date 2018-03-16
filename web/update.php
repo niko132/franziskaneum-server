3d3fbd093f01a15a76372f1b2e0953a18d650dff
niko132 <nikokirste@hotmail.de>
2018-03-16 16:45:08 +0100

    .

--- web/update.php
+++ web/update.php
@@ -2,6 +2,7 @@
 	if (isset($_POST['token']) && !empty($_POST['token'])) {		
 		$db_connection = pg_connect("host=ec2-54-75-227-92.eu-west-1.compute.amazonaws.com dbname=d7i5315uupgr2m user=mydxnmvauddyee password=493c42d4734c867c3fda62972ff79b1df2994deea5af051b3b1a4052e89f0381");
 		
+		$token = $_POST['token'];
 		$old_token = $_POST['old_token'];
 		$is_teacher = $_POST['is_teacher'];
 		$school_class = $POST['school_class'];
