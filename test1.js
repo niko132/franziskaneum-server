224e663e9da6e4d568e651b22c918c7f82b0d5cf
niko132 <nikokirste@hotmail.de>
2018-03-18 12:51:44 +0100

    .

--- test1.js
+++ test1.js
@@ -2,6 +2,18 @@ var request = require('request');
 var xml2js = require('xml2js');
 const { Client } = require('pg');
 
+function hasCourse(searchString, courses) {
+	for (var i = 0; i < courses.length; i++) {
+		var course = courses[i];
+		
+		if (searchString.indexOf(course) > -1) {
+			return true;
+		}
+	}
+	
+	return false;
+}
+
 var username = "FranzApp";
 var password = "Franz2017";
 var url = "http://franziskaneum.de/vplan/vplank.xml";
@@ -28,18 +40,50 @@ request(
 			
 			pgClient.query("SELECT * FROM users", (err, res) => {
 				for (var i = 0; i < res.rows.length; i++) {
-					var row = res.rows[i];
+					var user = res.rows[i];
 					
 					var userNot = '';
 					
-					for (var j = 0; j < haupt.aktion.length; j++) {
-						var aktion = haupt.aktion[j];
+					if (user.is_teacher) {
+						var shortcut = user.teacher_shortcut;
 						
-						if (aktion.klasse[0].indexOf(row.school_class + '/' + row.school_class_index) > -1) {
-							console.log(aktion.klasse[0] + ' ' + row.school_class + '/' + row.school_class_index);
+						for (var j = 0; j < haupt.aktion.length; j++) {
+							var aktion = haupt.aktion[j];
+							var searchString = aktion.lehrer[0] + aktion.info[0];
+						
+							if (searchString.indexOf(shortcut) > -1) {
+								console.log(searchString + ' ' + teacher_shortcut);
 							
-							userNot += aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.lehrer[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0];
-							userNot += '\n';
+								userNot += aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.klasse[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0];
+								userNot += '\n';
+							}
+						}
+					} else if (user.school_class >= 11) {
+						var schoolClass = user.school_class;
+						var courses = users.courses;
+						
+						for (var j = 0; j < haupt.aktion.length; j++) {
+							var aktion = haupt.aktion[j];
+						
+							if (aktion.klasse[0].indexOf(schoolClass) > -1 && hasCourse(aktion.klasse[0], courses)) {
+								console.log(aktion.klasse[0] + ' ' + schoolClass + ' ' + courses);
+							
+								userNot += aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.lehrer[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0];
+								userNot += '\n';
+							}
+						}
+					} else {
+						var schoolClass = user.school_class + ' ' + user.school_class_index;
+						
+						for (var j = 0; j < haupt.aktion.length; j++) {
+							var aktion = haupt.aktion[j];
+						
+							if (aktion.klasse[0].indexOf(schoolClass) > -1) {
+								console.log(aktion.klasse[0] + ' ' + schoolClass);
+							
+								userNot += aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.lehrer[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0];
+								userNot += '\n';
+							}
 						}
 					}
 					
