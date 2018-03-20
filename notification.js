915f4c888084d0fcbbec03af06757a96c2451bb2
niko132 <nikokirste@hotmail.de>
2018-03-20 20:00:46 +0100

    .

--- notification.js
+++ notification.js
@@ -110,7 +110,7 @@ request(
 						var hash = CRC32.str(userNotifications[j]);
 						userNotificationHashes.push(hash);
 						
-						console.log(Type(user.sended_notification_hashes));
+						console.log("Is Array: " + Array.isArray(user.sended_notification_hashes));
 						
 						if (user.sended_notification_hashes.indexOf(hash) > -1) {
 							newNotificationIndices.push(j);
