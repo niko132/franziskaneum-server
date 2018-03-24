8c2c8ffb7825e107653498888460e100f744716a
niko132 <nikokirste@hotmail.de>
2018-03-24 20:30:41 +0100

    .

--- clock.js
+++ clock.js
@@ -1,13 +1,15 @@
 var CronJob = require('cron').CronJob;
 var worker = require('./worker.js');
 
+var fifteenSeconsAfterMinute = function() {
+  console.log("Another minute is gone forever. Hopefully, you made the most of it...");
+}
+
 var job = new CronJob({
-	name: "Notification Scheduler",
-	cronTime: "*/5 * * * * *", // every 5 minutes
-	onTick: worker.start(),
-	start: true,
-	id: "notificationscheduler",
-	timeZone: "Europe/Berlin"
+  cronTime: "15 * * * * *",//15 seconds after every minute
+  onTick: fifteenSeconsAfterMinute,
+  start: true,
+  timeZone: "America/Los_Angeles"
 });
 
 job.start();
