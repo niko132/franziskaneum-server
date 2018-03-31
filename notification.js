ed60d001bc5e0b3c50951aae2236cf4d032f62b0
niko132 <nikokirste@hotmail.de>
2018-03-31 12:33:53 +0200

    .

--- notification.js
+++ notification.js
@@ -1,7 +1,9 @@
 var CRC32 = require('crc-32');
 var request = require('request');
 var xml2js = require('xml2js');
-const {Client} = require('pg');
+const {
+	Client
+} = require('pg');
 
 function hasCourse(searchString, courses) {
 	for (var i = 0; i < courses.length; i++) {
@@ -37,149 +39,148 @@ var main = function () {
 			const pgClient = new Client({
 					connectionString: process.env.DATABASE_URL,
 				});
-			pgClient.connect();
+			await pgClient.connect();
 
-			pgClient.query("SELECT * FROM users", (err, res) => {
-				for (var i = 0; i < res.rows.length; i++) {
-					var user = res.rows[i];
+			var res = await pgClient.query("SELECT * FROM users");
+			for (var i = 0; i < res.rows.length; i++) {
+				var user = res.rows[i];
 
-					if (!user.show_notifications) {
-						continue;
+				if (!user.show_notifications) {
+					continue;
+				}
+
+				var userNotifications = [];
+
+				var userNot = '';
+				var userNotCount = 0;
+
+				if (user.is_teacher) {
+					console.log('teacher user');
+
+					var shortcut = user.teacher_shortcut;
+
+					if (shortcut) {
+						for (var j = 0; j < haupt.aktion.length; j++) {
+							var aktion = haupt.aktion[j];
+							var searchString = aktion.lehrer[0] + ' ' + aktion.info[0];
+
+							if (searchString.indexOf(shortcut) > -1) {
+								userNotifications.push(aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.klasse[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0]);
+
+								userNot += aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.klasse[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0];
+								userNot += '\n';
+
+								userNotCount += 1;
+							}
+						}
 					}
+				} else {
+					var schoolClass = user.school_class;
 
-					var userNotifications = [];
+					if (schoolClass >= 11) {
+						console.log('sek2 user');
 
-					var userNot = '';
-					var userNotCount = 0;
+						var courses = user.courses;
 
-					if (user.is_teacher) {
-						console.log('teacher user');
-
-						var shortcut = user.teacher_shortcut;
-
-						if (shortcut) {
+						if (courses.length > 0) {
 							for (var j = 0; j < haupt.aktion.length; j++) {
 								var aktion = haupt.aktion[j];
-								var searchString = aktion.lehrer[0] + ' ' + aktion.info[0];
 
-								if (searchString.indexOf(shortcut) > -1) {
-									userNotifications.push(aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.klasse[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0]);
+								if (aktion.klasse[0].indexOf(schoolClass) > -1 && hasCourse(aktion.klasse[0], courses)) {
+									userNotifications.push(aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.lehrer[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0]);
 
-									userNot += aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.klasse[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0];
+									userNot += aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.lehrer[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0];
 									userNot += '\n';
 
 									userNotCount += 1;
 								}
 							}
 						}
-					} else {
-						var schoolClass = user.school_class;
+					} else if (schoolClass >= 5) { // default value von 0 vermeiden
+						console.log('sek1 user');
 
-						if (schoolClass >= 11) {
-							console.log('sek2 user');
+						var schoolClassIndex = user.school_class_index;
 
-							var courses = user.courses;
+						if (schoolClassIndex > 0) {
+							var schoolClassString = schoolClass + '/' + schoolClassIndex;
 
-							if (courses.length > 0) {
-								for (var j = 0; j < haupt.aktion.length; j++) {
-									var aktion = haupt.aktion[j];
+							for (var j = 0; j < haupt.aktion.length; j++) {
+								var aktion = haupt.aktion[j];
 
-									if (aktion.klasse[0].indexOf(schoolClass) > -1 && hasCourse(aktion.klasse[0], courses)) {
-										userNotifications.push(aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.lehrer[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0]);
+								if (aktion.klasse[0].indexOf(schoolClassString) > -1) {
+									userNotifications.push(aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.lehrer[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0]);
 
-										userNot += aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.lehrer[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0];
-										userNot += '\n';
+									userNot += aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.lehrer[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0];
+									userNot += '\n';
 
-										userNotCount += 1;
-									}
-								}
-							}
-						} else if (schoolClass >= 5) { // default value von 0 vermeiden
-							console.log('sek1 user');
-
-							var schoolClassIndex = user.school_class_index;
-
-							if (schoolClassIndex > 0) {
-								var schoolClassString = schoolClass + '/' + schoolClassIndex;
-
-								for (var j = 0; j < haupt.aktion.length; j++) {
-									var aktion = haupt.aktion[j];
-
-									if (aktion.klasse[0].indexOf(schoolClassString) > -1) {
-										userNotifications.push(aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.lehrer[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0]);
-
-										userNot += aktion.stunde[0] + '. St. ' + aktion.fach[0] + ' ' + aktion.lehrer[0] + ' ' + aktion.raum[0] + ' ' + aktion.info[0];
-										userNot += '\n';
-
-										userNotCount += 1;
-									}
+									userNotCount += 1;
 								}
 							}
 						}
-					}
-
-					var userNotificationHashes = [];
-					var newNotificationIndices = [];
-
-					for (var j = 0; j < userNotifications.length; j++) {
-						var hash = CRC32.str(userNotifications[j]);
-						userNotificationHashes.push(hash);
-
-						console.log(hash);
-
-						if (user.notification_hashes.indexOf(hash) <= -1) { // this hash wasn't already presented
-							newNotificationIndices.push(j);
-						}
-					}
-					
-					var hashesString = userNotificationHashes.join(",");
-
-					console.log("NEW NOTIFICATIONS: " + newNotificationIndices.length);
-
-					// insert 'userNotificationHashes' in database
-					console.log("query: " + hashesString);
-					/*
-					pgClient.query("UPDATE users SET notification_hashes = '{" + hashesString + "}' WHERE token = '" + user.token + "'", (err, res) => {
-						console.log(err);
-						console.log(res);
-						console.log("---------");
-					});
-					*/
-					
-					const res = await client.query("UPDATE users SET notification_hashes = '{" + hashesString + "}' WHERE token = '" + user.token + "'");
-					console.log("Token: " + user.token);
-
-					if (newNotificationIndices.length > 0) { // new notifications -> request fcm
-						var notificationCount = newNotificationIndices.length;
-
-						var body = {
-							to: user.token,
-							notification: {
-								body: notificationCount + ' Änderungen '
-							}
-						}
-
-						console.log(JSON.stringify(body));
-
-						request({
-							url: 'https://fcm.googleapis.com/fcm/send',
-							method: 'POST',
-							body: JSON.stringify(body),
-							headers: {
-								"Content-Type": "application/json",
-								"Authorization": "key=AIzaSyDCVVrr4nA3Pd6LmOWO7i0m95ASCTusw68"
-							}
-						},
-							function (error, response, body) {
-							console.log(body);
-
-							// TODO: Handle "InvalidRegistration" -> Delete Row
-						});
 					}
 				}
-				
-				await pgClient.end();
-			});
+
+				var userNotificationHashes = [];
+				var newNotificationIndices = [];
+
+				for (var j = 0; j < userNotifications.length; j++) {
+					var hash = CRC32.str(userNotifications[j]);
+					userNotificationHashes.push(hash);
+
+					console.log(hash);
+
+					if (user.notification_hashes.indexOf(hash) <= -1) { // this hash wasn't already presented
+						newNotificationIndices.push(j);
+					}
+				}
+
+				var hashesString = userNotificationHashes.join(",");
+
+				console.log("NEW NOTIFICATIONS: " + newNotificationIndices.length);
+
+				// insert 'userNotificationHashes' in database
+				console.log("query: " + hashesString);
+				/*
+				pgClient.query("UPDATE users SET notification_hashes = '{" + hashesString + "}' WHERE token = '" + user.token + "'", (err, res) => {
+				console.log(err);
+				console.log(res);
+				console.log("---------");
+				});
+				 */
+
+				const res1 = await pgClient.query("UPDATE users SET notification_hashes = '{" + hashesString + "}' WHERE token = '" + user.token + "'");
+				console.log("Token: " + user.token);
+
+				if (newNotificationIndices.length > 0) { // new notifications -> request fcm
+					var notificationCount = newNotificationIndices.length;
+
+					var body = {
+						to: user.token,
+						notification: {
+							body: notificationCount + ' Änderungen '
+						}
+					}
+
+					console.log(JSON.stringify(body));
+
+					request({
+						url: 'https://fcm.googleapis.com/fcm/send',
+						method: 'POST',
+						body: JSON.stringify(body),
+						headers: {
+							"Content-Type": "application/json",
+							"Authorization": "key=AIzaSyDCVVrr4nA3Pd6LmOWO7i0m95ASCTusw68"
+						}
+					},
+						function (error, response, body) {
+						console.log(body);
+
+						// TODO: Handle "InvalidRegistration" -> Delete Row
+					});
+				}
+			}
+
+			await pgClient.end();
 		});
 	});
 }
